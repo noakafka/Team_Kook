4f7069194ea5319c9f532274e81a8ad3ce99181f
NoaKafka <35343777+NoaKafka@users.noreply.github.com>
2020-11-11 11:36:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/teamkook/Fragment_B.kt
@@ -0,0 +1,25 @@
+package com.example.teamkook
+
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class Fragment_B(var c : Context) : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_b, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/teamkook/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.teamkook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeAsUpIndicator(R.mipmap.menu_icon)
+
+        var main_adapter = Main_pager_Adapter(supportFragmentManager)
+
+        VP.adapter = main_adapter
+        VP.offscreenPageLimit = 3
+
+        tabs_main.setupWithViewPager(VP)
+        tabs_main.getTabAt(0)?.setIcon(resources.getDrawable(R.mipmap.menu_icon))
+        tabs_main.getTabAt(1)?.setIcon(resources.getDrawable(R.mipmap.menu_icon))
+        tabs_main.getTabAt(2)?.setIcon(resources.getDrawable(R.mipmap.menu_icon))
+        tabs_main.getTabAt(3)?.setIcon(resources.getDrawable(R.mipmap.menu_icon))
+
+        tabs_main.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+            }
+        })
+
+
+    }
+
+    inner class Main_pager_Adapter : FragmentPagerAdapter {
+
+        var data1 : Fragment = Fragment_A()
+        var data2 : Fragment = Fragment_B(applicationContext)
+        var data3 : Fragment = Fragment_C(applicationContext)
+        var data4 : Fragment = Fragment_D(applicationContext)
+
+
+        var tab_data : ArrayList<Fragment> = arrayListOf(data1,data2,data3,data4)
+
+        constructor(fm : FragmentManager) : super(fm){
+
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return tab_data.get(position)
+        }
+
+        override fun getCount(): Int {
+            return tab_data.size
+        }
+    }
+}
